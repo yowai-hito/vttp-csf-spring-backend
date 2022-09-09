4f5eb823d1c98a18061a7631d1f53dca61982a96
yowai-hito <seow.h.y.95@gmail.com>
2022-09-09 09:54:08 +0800

    Springboot backend

---
+++ src/main/java/nus/vttp/csf/backend/SpringbootBackendForCsfApplication.java
@@ -0,0 +1,13 @@
+package nus.vttp.csf.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendForCsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendForCsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/vttp/csf/backend/SpringbootBackendForCsfApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.vttp.csf.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendForCsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
