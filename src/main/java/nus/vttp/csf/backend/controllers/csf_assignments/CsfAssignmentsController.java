0358bd90a8dd17c3e1922db0ca1c83080e98efad
yowai-hito <seow.h.y.95@gmail.com>
2022-09-09 13:46:35 +0800

    switch to json-smart from json-p

--- src/main/java/nus/vttp/csf/backend/controllers/csf_assignments/CsfAssignmentsController.java
+++ src/main/java/nus/vttp/csf/backend/controllers/csf_assignments/CsfAssignmentsController.java
@@ -1,8 +1,11 @@
 package nus.vttp.csf.backend.controllers.csf_assignments;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,18 +13,18 @@ import nus.vttp.csf.backend.models.csf_assignments.RegistrationRequest;
 import nus.vttp.csf.backend.services.csf_assignments.UserService;
 
 @RestController
-@RequestMapping(path="/csf/assignemnts")
+@RequestMapping(path="/csf/assignments")
 public class CsfAssignmentsController {
     
     private UserService userService;
 
     @Autowired
-    public CsfAssignmentsController(UserService userService) {
+    public CsfAssignmentsController(UserService userService) { 
         this.userService = userService;
     } 
 
     @PostMapping(path="/register")
-    public ResponseEntity<Object> doRegistration(RegistrationRequest req) {
+    public ResponseEntity<Object> doRegistration(@Valid @RequestBody RegistrationRequest req) {
         return this.userService.registerUser(req); 
     }
 
